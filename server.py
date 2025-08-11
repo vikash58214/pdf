24fa4c4eb3263333b9877384f40943b7b77f9d22
Vikash Sharma <vikash58214@gmail.com>
2025-08-12 01:03:32 +0530

    minor change

--- server.py
+++ server.py
@@ -40,4 +40,4 @@ def generate_pdf():
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 8000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port,debug=True)
